ad622adcd88face9287a5ae9bca4566cb5f77680
Mastersam <abadasamuelosp@gmail.com>
2020-03-27 00:34:18 -0700

    new screens

---
+++ lib/screens/statistics.dart
@@ -0,0 +1,14 @@
+import 'package:flutter/material.dart';
+import 'package:tojuwa/widgets/dev_scaffold.dart';
+
+class Statistics extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return DevScaffold(
+      title: 'Stats Area',
+      body: Center(
+        child: Text('Coming Soon'),
+      ),
+    );
+  }
+}
