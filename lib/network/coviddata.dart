e62a7027d2c6a1ed6de8dec6dd18f7c35c728efc
Mastersam <abadasamuelosp@gmail.com>
2020-03-27 22:24:19 -0700

    corrected data

--- lib/network/coviddata.dart
+++ lib/network/coviddata.dart
@@ -6,22 +6,21 @@ class CasesForCorona {
   List<Covid> cases = [];
 
   Future<void> getCasesForCorona() async {
-    String url =
-        "https://covid19.mathdro.id/api/countries/nigeria/confirmed";
+    String url = "https://covid19.mathdro.id/api/countries/nigeria/confirmed";
 
     var response = await http.get(url);
 
     var jsonData = jsonDecode(response.body);
 
     if (jsonData['status'] == "ok") {
-      jsonData.forEach((element) {
-        Covid gottencases = Covid(
-          confirmed: element['confirmed'],
-          recovered: element['recovered'],
-          deaths: element['deaths'],
-        );
-        cases.add(gottencases);
-      });
+      Covid gottencases = Covid(
+        confirmed: jsonData['confirmed']['value'],
+        recovered: jsonData['recovered']['value'],
+        deaths: jsonData['deaths']['value'],
+      );
+      cases.add(gottencases);
+    } else {
+      print(response.statusCode);
     }
   }
 }
@@ -30,22 +29,21 @@ class GlobalTotalForCorona {
   List<Covid> cases = [];
 
   Future<void> getTotalCasesForCorona() async {
-    String url =
-        "https://covid19.mathdro.id/api";
+    String url = "https://covid19.mathdro.id/api";
 
     var response = await http.get(url);
 
     var jsonData = jsonDecode(response.body);
 
     if (jsonData['status'] == "ok") {
-      jsonData.forEach((element) {
-        Covid gottencases = Covid(
-          confirmed: element['confirmed']['value'],
-          recovered: element['recovered']['value'],
-          deaths: element['deaths']['value'],
-        );
-        cases.add(gottencases);
-      });
+      Covid gottencases = Covid(
+        confirmed: jsonData['confirmed']['value'],
+        recovered: jsonData['recovered']['value'],
+        deaths: jsonData['deaths']['value'],
+      );
+      cases.add(gottencases);
+    } else {
+      print(response.statusCode);
     }
   }
 }
