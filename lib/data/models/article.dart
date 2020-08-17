2ac661130e444a289fcdde8ef3da4d85534dfc5f
Mastersam <abadasamuelosp@gmail.com>
2020-08-16 18:41:11 -0700

    models

---
+++ lib/data/models/article.dart
@@ -0,0 +1,82 @@
+class Article {
+  List<Articles> articles;
+
+  Article({this.articles});
+
+  Article.fromJson(Map<String, dynamic> json) {
+    if (json['articles'] != null) {
+      articles = new List<Articles>();
+      json['articles'].forEach((v) {
+        articles.add(new Articles.fromJson(v));
+      });
+    }
+  }
+
+  Map<String, dynamic> toJson() {
+    final Map<String, dynamic> data = new Map<String, dynamic>();
+    if (this.articles != null) {
+      data['articles'] = this.articles.map((v) => v.toJson()).toList();
+    }
+    return data;
+  }
+}
+
+class Articles {
+  Source source;
+  String title;
+  String description;
+  String url;
+  String urlToImage;
+  String publishedAt;
+  String content;
+
+  Articles(
+      {this.source,
+      this.title,
+      this.description,
+      this.url,
+      this.urlToImage,
+      this.publishedAt,
+      this.content});
+
+  Articles.fromJson(Map<String, dynamic> json) {
+    source =
+        json['source'] != null ? new Source.fromJson(json['source']) : null;
+    title = json['title'];
+    description = json['description'];
+    url = json['url'];
+    urlToImage = json['urlToImage'];
+    publishedAt = json['publishedAt'];
+    content = json['content'];
+  }
+
+  Map<String, dynamic> toJson() {
+    final Map<String, dynamic> data = new Map<String, dynamic>();
+    if (this.source != null) {
+      data['source'] = this.source.toJson();
+    }
+    data['title'] = this.title;
+    data['description'] = this.description;
+    data['url'] = this.url;
+    data['urlToImage'] = this.urlToImage;
+    data['publishedAt'] = this.publishedAt;
+    data['content'] = this.content;
+    return data;
+  }
+}
+
+class Source {
+  String name;
+
+  Source({this.name});
+
+  Source.fromJson(Map<String, dynamic> json) {
+    name = json['name'];
+  }
+
+  Map<String, dynamic> toJson() {
+    final Map<String, dynamic> data = new Map<String, dynamic>();
+    data['name'] = this.name;
+    return data;
+  }
+}
---
+++ lib/data/models/coviddata.dart
@@ -0,0 +1,54 @@
+class Covid {
+  Confirmed confirmed;
+  Confirmed recovered;
+  Confirmed deaths;
+  String lastUpdate;
+
+  Covid({this.confirmed, this.recovered, this.deaths, this.lastUpdate});
+
+  Covid.fromJson(Map<String, dynamic> json) {
+    confirmed = json['confirmed'] != null
+        ? new Confirmed.fromJson(json['confirmed'])
+        : null;
+    recovered = json['recovered'] != null
+        ? new Confirmed.fromJson(json['recovered'])
+        : null;
+    deaths =
+        json['deaths'] != null ? new Confirmed.fromJson(json['deaths']) : null;
+    lastUpdate = json['lastUpdate'];
+  }
+
+  Map<String, dynamic> toJson() {
+    final Map<String, dynamic> data = new Map<String, dynamic>();
+    if (this.confirmed != null) {
+      data['confirmed'] = this.confirmed.toJson();
+    }
+    if (this.recovered != null) {
+      data['recovered'] = this.recovered.toJson();
+    }
+    if (this.deaths != null) {
+      data['deaths'] = this.deaths.toJson();
+    }
+    data['lastUpdate'] = this.lastUpdate;
+    return data;
+  }
+}
+
+class Confirmed {
+  int value;
+  String detail;
+
+  Confirmed({this.value, this.detail});
+
+  Confirmed.fromJson(Map<String, dynamic> json) {
+    value = json['value'];
+    detail = json['detail'];
+  }
+
+  Map<String, dynamic> toJson() {
+    final Map<String, dynamic> data = new Map<String, dynamic>();
+    data['value'] = this.value;
+    data['detail'] = this.detail;
+    return data;
+  }
+}
